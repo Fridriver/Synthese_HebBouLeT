048cea764a03211e1121b3ca58823c7c6478eb18
Jean-Michel <jeanmi790@hotmail.com>
2023-11-17 11:48:00 -0500

    UI

---
+++ Assets/_MyAssets/Scripts/UI/CanvasManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPrincipal = default;
+    [SerializeField] private GameObject menuMultijoueur;
+    //LIST_SCENES[1] = "Niveau"
+
+    
+         
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuMultijoueur.SetActive(false);
+        menuPrincipal.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneLoaderManager.Instance.LoadScene(SceneLoaderManager.LIST_SCENES[1]);
+    }
+
+    public void Multiplayer()
+    {
+        menuPrincipal.SetActive(false);
+        menuMultijoueur.SetActive(true);
+        //Initialiser la connection au serveur Unity pour le multijoueur 
+    }
+
+   public void Quit()
+    {
+        Application.Quit();
+    }
+}
