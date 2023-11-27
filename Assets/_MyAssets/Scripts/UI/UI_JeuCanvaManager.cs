866f524592ad949cc67662502dfa04537b4eeb82
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2023-11-27 14:47:25 -0500

    Ajout de montre
    https://www.freepik.com/3d-model/smartwatch-001_3667.htm#query=watch&position=7&from_view=search&uuid=3c5c13f6-836e-4a2e-9a3e-7cdcadff9704

---
+++ Assets/_MyAssets/Scripts/UI/UI_JeuCanvaManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_JeuCanvaManager : MonoBehaviour
+{
+    [Header("Canvas")]
+    [SerializeField] private GameObject UI_InGameCanvas;
+
+    [Header("Panels")]
+    [SerializeField] private GameObject PanelMenuPrincipal;
+    [SerializeField] private GameObject PanelOptions;
+
+    [Header("Boutons pour ouvrir Panel")]
+    [SerializeField] private Button btnOptions;
+    [SerializeField] private Button btnQuitter;
+
+    [Header("Boutons de retour")]
+    [SerializeField] private Button btnRetourOptions;
+
+    private GameObject[] listePanels;
+    private bool MenuActif = false;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI_InGameCanvas.SetActive(false);
+
+        listePanels = new GameObject[] { PanelMenuPrincipal, PanelOptions };
+
+        ActivateRightPanel(0);
+
+
+        btnOptions.onClick.AddListener(() => ActivateRightPanel(1));
+        btnRetourOptions.onClick.AddListener(() => ActivateRightPanel(0));
+        btnQuitter.onClick.AddListener(()=>Quit());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ActivateRightPanel(int index)
+    {
+        for (int i = 0; i < listePanels.Length; i++)
+        {
+            {
+                listePanels[i].SetActive(i == index);
+            }
+        }
+
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
