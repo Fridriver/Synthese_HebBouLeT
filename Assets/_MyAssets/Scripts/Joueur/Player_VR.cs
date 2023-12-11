367183e46b6deab8d2e1315dbf84885ff9934841
Fridriver <117280045+Fridriver@users.noreply.github.com>
2023-12-10 22:52:03 -0500

    Barre et Alerte et vie

--- Assets/_MyAssets/Scripts/Joueur/Player_VR.cs
+++ Assets/_MyAssets/Scripts/Joueur/Player_VR.cs
@@ -1,35 +1,67 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player_VR : MonoBehaviour
 {
+    private AudioSource audioSource;
+
     [Header("Stats Joueur")]
     [SerializeField] private float _gainSante = 20;
-    [SerializeField] private float _santePerteSec = 0.004f;
-    [SerializeField] private float _maxSante = 100;
+    [SerializeField] private float _santePerteSec = 0.5f;
+    [SerializeField] private float _maxSante = 1000f;
+    [SerializeField] private Image healthBar;
+    [SerializeField] private AudioClip alertHealthLow;
     private float _sante;
+    private bool isProgression = false;
+    private bool isAlert = false;
+
     [Header("Zone de mort")]
     [SerializeField] private Collider _zoneMort = default;
 
+    
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         _sante = _maxSante;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Maladie();
-        //Debug.Log(_sante);
+        Maladie();
+        Alert();
     }
 
 
     private void Maladie()
     {
-        _sante -= _santePerteSec;
+        if (!isProgression)
+        {
+            isProgression = true;
+            if (_sante != 0)
+            {
+                StartCoroutine(DiseaseProgression());
+                _sante--;
+                healthBar.fillAmount = _sante / _maxSante;
+                Debug.Log(_sante);
+            }
+        }
+    }
+
+    private void Alert()
+    {
+        if (_sante <= (0.25 *_maxSante))
+        {
+            if (!isAlert)
+            {
+                isAlert = true;
+                StartCoroutine(AlertWait());
+                audioSource.PlayOneShot(alertHealthLow);
+                Debug.Log("Alerte !!!");
+            }
+        }
     }
 
     public void BlobHit()
@@ -46,14 +78,13 @@ public class Player_VR : MonoBehaviour
         }
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.tag == "Ennemi")
-    //    {
-    //        PlayerDead();
-    //    }
-    //}
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ennemi")
+        {
+            PlayerDead();
+        }
+    }
 
     private void PlayerDead()
     {
@@ -64,5 +95,17 @@ public class Player_VR : MonoBehaviour
     IEnumerator DeadWait()
     {
         yield return new WaitForSeconds(5f);
+    }
+
+    IEnumerator DiseaseProgression()
+    {
+        yield return new WaitForSeconds(_santePerteSec);
+        isProgression = false;
+    }
+
+    IEnumerator AlertWait()
+    {
+        yield return new WaitForSeconds(alertHealthLow.length + 2);
+        isAlert = false;
     }
 }
